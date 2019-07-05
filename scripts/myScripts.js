2444418215a11ee1c5c9a142f9dcbb61d5daef97
sternandburn <51689230+sternandburn@users.noreply.github.com>
2019-07-04 22:55:33 -0500

    Add files via upload

---
+++ scripts/myScripts.js
@@ -0,0 +1,7 @@
+function functionexample() {
+var person = prompt("Please enter your name", "Noah Stern");
+if (person != null) {
+		alert("Hello! What's Up?");
+		console.log("Hello! What's Up?");
+	}
+}
